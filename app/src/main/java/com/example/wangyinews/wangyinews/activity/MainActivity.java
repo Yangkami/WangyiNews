a0deb2e32c1928dc03a9e69cf920186fc98a7c1a
EXDLS <3198572723@qq.com>
2018-07-09 11:41:10 +0800

    update

---
+++ app/src/main/java/com/example/wangyinews/wangyinews/activity/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.wangyinews.wangyinews.activity;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RadioGroup;
+
+import com.example.wangyinews.wangyinews.R;
+import com.example.wangyinews.wangyinews.pager.BasePager;
+import com.example.wangyinews.wangyinews.pager.MinePager;
+import com.example.wangyinews.wangyinews.pager.RadioPager;
+import com.example.wangyinews.wangyinews.adapter.MainPagerAdapter;
+import com.example.wangyinews.wangyinews.pager.LivePager;
+import com.example.wangyinews.wangyinews.pager.NewsPager;
+import com.example.wangyinews.wangyinews.pager.VideoPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, ViewPager.OnPageChangeListener {
+
+    private  static final String TAG="";
+    private ViewPager vpMain;
+    private RadioGroup rgMain;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG,"-----");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vpMain=findViewById(R.id.vp_main);
+        rgMain=findViewById(R.id.rg_main);
+
+        rgMain.setOnCheckedChangeListener(this);
+        vpMain.setOnPageChangeListener(this);
+
+        //rgMain.setOnCheckedChangeListener(this);
+
+        initData();
+
+    }
+    private void initData(){
+        List<BasePager> pagers=new ArrayList<>();
+        pagers.add(new NewsPager(this));
+        pagers.add(new VideoPager(this));
+        pagers.add(new LivePager(this));
+        pagers.add(new RadioPager(this));
+        pagers.add(new MinePager(this));
+
+        //适配器
+        MainPagerAdapter mAdapter = new MainPagerAdapter(pagers);
+        //配置适配器
+
+        vpMain.setAdapter(mAdapter);
+
+        pagers.get(0).initData(null);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        int index = -1;
+        switch (checkedId){
+            case R.id.btn_news:
+                index =0;
+                break;
+            case R.id.btn_video:
+                index =1;
+                break;
+            case R.id.btn_live:
+                index =2;
+                break;
+            case R.id.btn_audio:
+                index =3;
+                break;
+            case R.id.btn_mine:
+                index =4;
+                break;
+        }
+        Log.i(TAG,"---index=" +index);
+        if(index>=0){
+            vpMain.setCurrentItem(index);
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        Log.w(TAG,"----------onPageScrolled"+position);
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.i(TAG,"----------posotoion"+position);
+        int resid=rgMain.getChildAt(position).getId();
+        rgMain.check(resid);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
---
+++ app/src/main/java/com/example/wangyinews/wangyinews/pager/BasePager.java
@@ -0,0 +1,73 @@
+package com.example.wangyinews.wangyinews.pager;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.wangyinews.wangyinews.R;
+
+
+//import butterknife.ButterKnife;
+
+/**
+ * 主页左侧页面父类
+ * Created by pan on 2017/3/4.
+ */
+public abstract class BasePager {
+
+    public static final String TAG = "BasePager";
+
+    public Context mContext;
+    public View mRootView;// 布局对象
+    protected TextView tvCenter;
+
+
+    protected LayoutInflater lf;
+
+
+    public BasePager(Context context) {
+        mContext = context;
+        lf = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mRootView = lf.inflate(loadLayoutById(), null);
+
+//        ButterKnife.bind(mRootView);
+
+
+        initView();
+    }
+
+    protected int loadLayoutById() {
+
+        return R.layout.pager_main;
+    }
+
+    private void initView() {
+//        tvCenter = mRootView.findViewById(R.id.tv_center_main);
+        //加载子类view
+        initViews();
+    }
+
+    /**
+     * 初始化布局
+     */
+    public abstract void initViews();
+
+    public abstract void initData(Object data);
+
+
+    /***
+     * 无数据默认加载
+     */
+    public void loadViews() {
+
+    }
+
+
+    public String getResString(int resId) {
+        return mContext.getResources().getString(resId);
+    }
+
+
+
+}
